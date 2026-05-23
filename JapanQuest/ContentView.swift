d53f900ca617d7af232d9216da7f4614e6ea094d
JapanQuest <ne241003@senshu-u.jp>
2026-05-23 19:10:34 +0900

    Initial Commit

---
+++ JapanQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JapanQuest
+//
+//  Created by 髙木敬太 on 2026/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JapanQuest/JapanQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapanQuestApp.swift
+//  JapanQuest
+//
+//  Created by 髙木敬太 on 2026/05/23.
+//
+
+import SwiftUI
+
+@main
+struct JapanQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
